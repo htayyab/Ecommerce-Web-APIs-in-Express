0167febbe8d15d481664bdb7f2425f7c3f50ca63
Talha6891 <raotalha6891@gmail.com>
2024-08-16 17:40:15 +0500

    initial setup

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import { register, login } from '../controllers/authController.js';
-import registerValidation from '../middlewares/validations/registerValidation.js';
+import {registerValidation, loginValidation } from '../middlewares/validations/registerValidation.js';
 
 const router = express.Router();
 
 router.post('/register',registerValidation,register);
+
+router.post('/login',loginValidation,login);
 
 export default router;
